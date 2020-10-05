6fbea4235108d1476887251f02f642a258c87297
benjlev <levitas.ben@gmail.com>
2020-10-05 16:34:25 +0300

    Added/edited documentation

--- alpaca_handler/data.py
+++ alpaca_handler/data.py
@@ -23,16 +23,27 @@ class Data:
         The Alpaca REST API
     _market_status : str
         An indicator of the market status as of last time checked
+    is_market_open : bool
+        True/False according to current market status
 
     Methods
     -------
-    is_market_open()
-        return 'open' or 'closed' according to current market status
-
+    await_open()
+        wait for open market hours
+    await_close()
+        wait for market close
+    set_symbols()
+        set symbols after initialization
+    wait_for_market_status_change()
+        wait for market status change: open->close or close->open
+    get_bars()
+        get bars for relevant symbols
     """
     def __init__(self, key: str, secret_key: str, symbols: list, time_frame: str = '1D'):
         """
-        :param symbols: The list of symbols
+        :param key: Alpaca API key
+        :param secret_key: Alpaca API secret key
+        :param symbols: List of symbols
         :param time_frame: Time frame between bars
         """
         self._symbols = list(map(lambda x: x.upper(), symbols))
@@ -51,29 +62,39 @@ class Data:
     def is_market_open(self) -> bool:
         """
         Returns current market status.
-        :return: (str) 'open' or 'closed'
+        :return: Boolean
         """
         return self._live_api.get_clock().is_open
 
     def _wait_until(self, future_time: pd.Timestamp):
+        """
+        Sleep until the given time
+        :param future_time: given time
+        """
         now = self._live_api.get_clock().timestamp
         delta = (future_time - now).total_seconds()
         if delta > 0:
             time.sleep(delta + 1)
 
     def await_open(self):
+        """Awaits open market"""
         self._wait_until(self._live_api.get_clock().next_open)
 
     def await_close(self):
+        """Awaits closed market"""
         self._wait_until(self._live_api.get_clock().next_close)
 
     def set_symbols(self, symbols):
+        """
+        Sets symbols after initialization
+        :param symbols:
+        """
         self._symbols = list(map(lambda x: x.upper(), symbols))
 
     def wait_for_market_status_change(self) -> bool:
         """
-        Waits for market status change and then returns the new market status.
-        :return: (str) 'open' or 'closed'
+        Waits for market status change.
+        :return: market status: open -> True, closed -> False
         """
         while True:
             if self._market_status != self.is_market_open:
@@ -85,10 +106,10 @@ class Data:
 
     def get_bars(self, limit: int = 20, columns: list = None) -> pd.DataFrame:
         """
-        Gets full bar sets for relevant symbols.
-        :param limit: amount of days to look at (including current day)
+        Gets full bar sets for self's symbols.
+        :param limit: amount of days to pull (includes today)
         :param columns: relevant df columns
-        :return: (dict) Full bar sets
+        :return: Full bar sets
         """
         bar_set = self._live_api.get_barset(self._symbols, self._time_frame, limit=limit)
         df = bar_set.df
--- alpaca_handler/portfolio.py
+++ alpaca_handler/portfolio.py
@@ -9,7 +9,41 @@ logger = logging.getLogger()
 
 
 class Portfolio:
+    """
+    A class for handling Alpaca portfolio trades and orders
+
+    ...
+
+    Attributes
+    ----------
+    _alpaca_api : alpaca.REST
+        Alpaca API connnection
+    is_market_open: bool
+        return True/False according to current market status
+    positions: Dict[str, Position]
+        return order positions
+
+    Methods
+    -------
+    get_current_qty()
+        Get stock holding quantities
+    open_orders()
+        Get stock list with open orders
+    get_buying_power()
+        Get available buying power
+    trade()
+        Stock buy or sell
+    liquidate()
+        Liquidate stock holdings
+    liquidate_all()
+        Liquidate all stock holdings
+    """
     def __init__(self, key: str, secret_key: str, staging: bool = True):
+        """
+        :param key: Alpaca API key
+        :param secret_key: Alpaca API secret key
+        :param staging: True for paper trading, False for live trading
+        """
         headers = {
             'key_id': key,
             'secret_key': secret_key,
@@ -23,14 +57,27 @@ class Portfolio:
 
     @property
     def is_market_open(self) -> property:
+        """
+        Returns current market status.
+        :return: Boolean
+        """
         return Data.is_market_open
 
     @property
     def positions(self) -> Dict[str, Position]:
+        """
+        :return: Dict(symbol: positions)
+        """
         positions_list = self._alpaca_api.list_positions()
         return {position.symbol: position for position in positions_list}
 
-    def submit_order(self, symbol: str, amount: int, trade_type: str) -> None:
+    def _submit_order(self, symbol: str, amount: int, trade_type: str) -> None:
+        """
+        Submits stock buy/sell
+        :param symbol: Stock symbol
+        :param amount: Buy quantity
+        :param trade_type: 'buy'/'sell'
+        """
         try:
             self._alpaca_api.submit_order(symbol, amount, trade_type, 'market', 'day')
             #logger.trade(symbol, trade_type, amount)
@@ -38,6 +85,11 @@ class Portfolio:
             logger.debug(f"{symbol} - order submission unsuccessful - {exc}")
 
     def get_current_qty(self, symbols: List[str]) -> Dict[str, int]:
+        """
+        Get stock holding quantities
+        :param symbols: Stock symbols
+        :return: Dict(symbol: quantity)
+        """
         quantities = {}
         for symbol in symbols:
             try:
@@ -46,19 +98,31 @@ class Portfolio:
                 quantities[symbol] = 0
         return quantities
 
-    def open_orders(self):
+    def open_orders(self) -> list:
+        """:return: symbols that have open orders"""
         orders = self._alpaca_api.list_orders(status='open')
         return list(map(lambda order: order.symbol, orders))
 
     def get_buying_power(self) -> float:
+        """:return: Available buying power"""
         return float(self._alpaca_api.get_account().buying_power)
 
     def trade(self, symbol: str, amount: int, trade_type: str):
+        """
+        Stock buy/sell
+        :param symbol: Stock symbol
+        :param amount: buy/sell quantity
+        :param trade_type: 'buy' or 'sell'
+        """
         if 0 < self.get_buying_power():
             logger.debug(f"{symbol} - submitting order")
-            self.submit_order(symbol, amount, trade_type)
+            self._submit_order(symbol, amount, trade_type)
 
     def liquidate(self, symbols: List[str]):
+        """
+        Liquidate stock holdings
+        :param symbols: Stock symbols
+        """
         for symbol in symbols:
             try:
                 self._alpaca_api.close_position(symbol)
@@ -68,5 +132,6 @@ class Portfolio:
                 #logger.exception(exc)
 
     def liquidate_all(self):
+        """Liquidate all stock holdings"""
         self._alpaca_api.close_all_positions()
         logger.info("Sold all positions")
